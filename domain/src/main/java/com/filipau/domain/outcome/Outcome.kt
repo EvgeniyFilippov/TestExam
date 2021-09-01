5a95cf059d8c322c13eda98ca6cb923700ee89e7
yauhen filipau <6479556@gmail.com>
2021-09-01 21:28:53 +0300

    add utils for outcome

---
+++ domain/src/main/java/com/filipau/domain/outcome/Outcome.kt
@@ -0,0 +1,25 @@
+package com.filipau.domain.outcome
+
+import java.io.Serializable
+
+sealed class Outcome<T> : Serializable {
+
+	data class Progress<T>(var loading: Boolean) : Outcome<T>()
+	data class Success<T>(var data: T) : Outcome<T>()
+	data class Next<T>(var data: T) : Outcome<T>()
+	data class Failure<T>(val e: Throwable) : Outcome<T>()
+
+	companion object {
+		fun <T> loading(isLoading: Boolean): Outcome<T> =
+			Progress(isLoading)
+
+		fun <T> success(data: T): Outcome<T> =
+			Success(data)
+
+		fun <T> failure(e: Throwable): Outcome<T> =
+			Failure(e)
+
+		fun <T> next(data: T): Outcome<T> =
+			Next(data)
+	}
+}
