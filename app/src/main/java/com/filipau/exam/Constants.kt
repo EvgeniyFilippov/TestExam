d0b0fca7a14b645c0170c38ae28c1d9879339c46
yauhen filipau <6479556@gmail.com>
2021-09-01 21:29:06 +0300

    update constants

--- app/src/main/java/com/filipau/exam/Constants.kt
+++ app/src/main/java/com/filipau/exam/Constants.kt
@@ -2,6 +2,11 @@ package com.filipau.exam
 
 object Constants {
 
+    //LOGS
     const val LOGCAT_FILE = "logcat.txt"
     const val LOG_DIR = "Log_File"
+
+    //LIVE DATA
+    const val ALL_POSTS_LIVE_DATA = "allPostsLiveData"
+    const val ALL_USERS_LIVE_DATA = "allUsersLiveData"
 }
