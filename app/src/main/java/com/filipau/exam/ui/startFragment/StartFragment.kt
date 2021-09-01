88fe4e5ab08a75b7770b642460737185b7f1cd6e
yauhen filipau <6479556@gmail.com>
2021-09-01 17:26:40 +0300

    add animate

--- app/src/main/java/com/filipau/exam/ui/startFragment/StartFragment.kt
+++ app/src/main/java/com/filipau/exam/ui/startFragment/StartFragment.kt
@@ -4,17 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.fragment.app.Fragment
 import com.filipau.exam.R
 import com.filipau.exam.base.BaseMvvmView
 import com.filipau.exam.databinding.FragmentStartBinding
 import com.filipau.exam.ext.showAlertDialog
 
-
 open class StartFragment : Fragment(R.layout.fragment_start), BaseMvvmView {
 
     private var binding: FragmentStartBinding? = null
     private val viewModel: StartViewModel = StartViewModel()
+    private var tractor: AppCompatImageView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,9 +28,19 @@ open class StartFragment : Fragment(R.layout.fragment_start), BaseMvvmView {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        tractor = binding?.tractor
+
         binding?.btnSaveLogcat?.setOnClickListener {
             context?.let { it1 -> viewModel.writeLogCat(it1) }
         }
+
+        tractor = binding?.tractor
+        tractor?.alpha = .0f
+        tractor?.animate()
+            ?.setDuration(5000)
+            ?.alpha(1f)
+            ?.withEndAction { binding?.btnSaveLogcat?.visibility = View.VISIBLE }
+            ?.start()
 
     }
 
