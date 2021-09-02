e65aa5c135a8cec224dc64f570aba9a16cbc2a7b
yauhen filipau <6479556@gmail.com>
2021-09-02 16:34:41 +0300

    add custom toolbar

--- app/src/main/java/com/filipau/exam/ui/userFragment/UserFragment.kt
+++ app/src/main/java/com/filipau/exam/ui/userFragment/UserFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.lifecycleScope
@@ -16,6 +17,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.filipau.domain.outcome.Outcome
 import com.filipau.exam.Constants.ERROR
 import com.filipau.exam.Constants.ID_POST_KEY
+import com.filipau.exam.MainActivity
 import com.filipau.exam.R
 import com.filipau.exam.base.mvvm.BaseMvvmView
 import com.filipau.exam.databinding.FragmentUserBinding
@@ -33,18 +35,26 @@ class UserFragment : ScopeFragment(R.layout.fragment_user), BaseMvvmView {
     private lateinit var postId: String
     private val viewModel: UserViewModel by stateViewModel()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val toolbar = binding?.toolbar
+        (requireActivity() as MainActivity).setSupportActionBar(toolbar)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentUserBinding.inflate(inflater, container, false)
         postId = arguments?.getString(ID_POST_KEY) ?: ERROR
+
         return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding?.userId?.text = postId
         viewModel.getNewsFlow(postId).asLiveData(lifecycleScope.coroutineContext)
             .observe(viewLifecycleOwner, {
                 when (it) {
